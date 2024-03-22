ea0a9d05bbbcb04e0666c4117810d42f26f16219
1am-programmer <dannywebexpertise@gmail.com>
2024-03-22 13:39:32 -0800

    Apostrophe

--- src/components/HomeHero.jsx
+++ src/components/HomeHero.jsx
@@ -8,7 +8,7 @@ const HomeHero = () => {
         <div className=" bg-black absolute inset-0 opacity-35"></div>
       </div>
       <div className="relative z-20 -translate-y-[80px] flex flex-col items-center gap-4">
-        <p className="text-gray-400 text-2xl">Want to know what's hot?</p>
+        <p className="text-gray-400 text-2xl">Want to know what&apos;s hot?</p>
         <h1 className="font-bold text-5xl text-white">Spring Lookbook 2019</h1>
         <button className="border border-[#c96] text-white px-4 py-2 hover:bg-[#c96] flex gap-2 items-center ">
           START SCROLLING
