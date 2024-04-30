e53335c424b24d0999cd56b07ed2b910f627518b
Your Name <dannydotdev@gmail.com>
2024-04-30 16:36:07 +0100

    modified login and register page

--- src/app/auth/page.jsx
+++ src/app/auth/page.jsx
@@ -26,10 +26,7 @@ const page = () => {
         <TabsContent value="login" className="flex-1">
           <Card>
             <CardHeader>
-              <CardTitle>Sign In</CardTitle>
-              <CardDescription>
-                {/* Make changes to your account here. Click save when you're done. */}
-              </CardDescription>
+              <CardTitle className="text-xl -my-2">Login</CardTitle>
             </CardHeader>
             <CardContent className="space-y-2">
               <div className="space-y-1">
@@ -57,7 +54,7 @@ const page = () => {
                 />
               </div>
 
-              <div>
+              <div className="flex justify-between items-center py-5">
                 <Button className="bg-white rounded-none hover:bg-[#c96] hover:text-white font-normal border border-[#c96] text-[#c96] px-4 py-2 flex items-center gap-2">
                   LOG IN <FaArrowRight />
                 </Button>
@@ -67,34 +64,30 @@ const page = () => {
                     <input type="checkbox" />
                     <p>Remember Me</p>
                   </div>
-                  <p className="text-[12px] text-[#c96] flex justify-center">
-                    Forgotten password?
-                  </p>
                 </div>
+                <p className=" text-[#c96] flex justify-center">
+                  Forgotten password?
+                </p>
               </div>
+              <hr className="px-5" />
             </CardContent>
 
-            <CardFooter className=" flex justify-between"></CardFooter>
-            <hr className="px-5" />
-            <div>
-              <button className="border px-3 py-2 flex gap-2 items-center">
-                <FaGoogle className="text-red-700" />
+            <CardFooter className=" flex justify-between">
+              <button className="border px-3 py-2 flex gap-2 items-center hover:bg-red-700 rounded-md hover:text-white group">
+                <FaGoogle className="text-red-700 group-hover:text-white" />
                 Login with Google
               </button>
-            </div>
-            <div>
-              <button className="border px-3 py-2 flex gap-2 items-center ">
-                <FaFacebookF className="text-blue-500" />
+              <button className="border px-3 py-2 flex gap-2 items-center hover:bg-blue-700 rounded-md hover:text-white group">
+                <FaFacebookF className="text-blue-500 group-hover:text-white" />
                 Login With Facebook
               </button>
-            </div>
+            </CardFooter>
           </Card>
         </TabsContent>
         <TabsContent value="register">
           <Card>
             <CardHeader>
-              <CardTitle>Register</CardTitle>
-              <CardDescription></CardDescription>
+              <CardTitle className="text-xl -my-2">Register</CardTitle>
             </CardHeader>
             <CardContent className="space-y-2">
               <div className="space-y-1">
@@ -121,21 +114,31 @@ const page = () => {
                   className="bg-gray-200 outline-[#c96] border border-gray-300"
                 />
               </div>
-            </CardContent>
-            <CardFooter className=" flex justify-between">
-              <Button className="bg-white rounded-none hover:bg-[#c96] hover:text-white font-normal border border-[#c96] text-[#c96] px-4 py-2 flex items-center gap-2">
-                LOG IN <FaArrowRight />
-              </Button>
 
-              <div>
-                <div className="flex items-center gap-2 ">
-                  <input type="checkbox" />
-                  <p>Remember Me</p>
+              <div className="flex justify-between items-center py-5">
+                <Button className="bg-white rounded-none hover:bg-[#c96] hover:text-white font-normal border border-[#c96] text-[#c96] px-4 py-2 flex items-center gap-2">
+                  Sign up <FaArrowRight />
+                </Button>
+
+                <div>
+                  <div className="flex items-center gap-2 ">
+                    <input type="checkbox" />
+                    <p>Remember Me</p>
+                  </div>
                 </div>
-                <p className="text-[12px] text-[#c96] flex justify-center">
-                  Forgotten password?
-                </p>
               </div>
+              <hr className="px-5" />
+            </CardContent>
+
+            <CardFooter className=" flex justify-between">
+              <button className="border px-3 py-2 flex gap-2 items-center hover:bg-red-700 rounded-md hover:text-white group">
+                <FaGoogle className="text-red-700 group-hover:text-white" />
+                Sign up with Google
+              </button>
+              <button className="border px-3 py-2 flex gap-2 items-center hover:bg-blue-700 rounded-md hover:text-white group">
+                <FaFacebookF className="text-blue-500 group-hover:text-white" />
+                Sign up With Facebook
+              </button>
             </CardFooter>
           </Card>
         </TabsContent>
